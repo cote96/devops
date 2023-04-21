64c207f74199540d33e77669b02cda26d313cf43
RIKKIECK <rame.chile@gmail.com>
2023-04-20 21:01:02 -0400

    Initial

---
+++ src/test/java/com/tempdevops/demo/DemoDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempdevops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
